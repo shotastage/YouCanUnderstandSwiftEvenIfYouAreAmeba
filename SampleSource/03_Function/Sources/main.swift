2407f8534e57f749d1b4b9d665b85702a9dce497
Shota Shimazu <shota-shimazu.eis@keio.jp>
2016-08-31 00:07:17 +0900

    Add new sample code.

---
+++ SampleSource/03_Function/Sources/main.swift
@@ -0,0 +1,19 @@
+
+
+
+func greet() {
+    print("Hello, swift!")
+}
+
+
+func yourName(name: String) {
+    print("Your name is \(name).")
+}
+
+func variousHello(lang: String) -> String {
+    let hello: String
+    return hello
+}
+
+greet()
+yourName()
